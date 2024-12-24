254f9b678152c1c918075e89ef36726c104bdb5d
whereyoucanfindme <anishchy501625@gmail.com>
2024-12-24 22:06:51 +0545

    done

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,44 @@
+import streamlit as st 
+import pickle 
+import requests
+
+movies = pickle.load(open("movies_list.pk1", "rb"))
+similarity = picke.load(open("similarity.pk1", "rb"))
+movies_list = movies['title'].values
+
+st.header("Movie Recommender System")
+selectvalue = st.selectbox("Select movie from dropdown", movies_list)
+
+def fetch_poster(movie_id):
+    url = ""
+    requests.get(url)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https"+poster_path
+    return full_path
+
+def recommend(movie):
+    index = movies[movies['title']==movies].index[0]
+    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
+    recommend_movie = []
+    recommend_poster = []
+    for i in distance[1:6]:
+        recommend_movie.append(movies.iloc[i[0]].title))
+    return recommend_movie
+
+if st.button("Show Recommend"):
+    movie_name = recommend(selectvalue)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(movie_name[0])
+    with col2:
+        st.text(movie_name[1])
+    with col3:
+        st.text(movie_name[2])
+    with col4:
+        st.text(movie_name[3])
+    with col5:
+        st.text(movie_name[4])
+    
+        
